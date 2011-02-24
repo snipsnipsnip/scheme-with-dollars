550b9f62c1ff9b2d2be27b272d48d7c84027142f
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2011-02-24 16:38:17 +0900

    lisp

--- Main.hs
+++ Main.hs
@@ -40,8 +40,22 @@ anyChar = P $ do
 char :: Char -> P Char
 char c = do
     d <- anyChar
-    unless (c == d) $ fail $ "expected " ++ show c ++ ", but got " ++ show d
+    unless (c == d) $ fail $ ($ "") $
+        showString "expected char " .
+        showChar c .
+        showString ", but got " .
+        showChar d
     return d
+
+charExcept :: String -> P Char
+charExcept set = do
+    c <- anyChar
+    unless (c `notElem` set) $ fail $ ($ "") $
+        showString "expected char except " .
+        shows set .
+        showString ", but got " .
+        showChar c
+    return c
 
 ($>) :: Alternative f => f a -> b -> f b
 ($>) = flip (<$)
@@ -90,31 +104,118 @@ test = runP expr "1 + 1 * (6/9 )* 3 + 4" == Right (7, "")
 
 ------------
 
-newtype S = S (Either [S] Atom)
-type Atom = Either String Double
+data S
+    = L [S]
+    | Q S
+    | A Atom
+    deriving Eq
+
+data Atom
+    = Str String
+    | Sym String
+    | Num Double
+    deriving Eq
 
 instance Show S where
-    showsPrec _ (S s) = case s of
-        Left list -> showParen True $ foldl (.) id $ intersperse (showChar ' ') $ map shows list
-        Right (Left atom) -> shows atom
-        Right (Right atom) -> shows atom
+    showsPrec _ s = case s of
+        L list -> showParen True $ foldl (.) id $ intersperse (showChar ' ') $ map shows list
+        Q s -> showChar '\'' . shows s
+        A a -> shows a
+
+instance Show Atom where
+    showsPrec _ a = case a of
+        Str s -> shows s
+        Sym s -> showString s
+        Num n -> shows n
+
+stringEscapable :: Char -> Char -> P String
+stringEscapable marker escape = paren marker marker $ many chr
+    where
+    chr = quotedChr <|> normalChr
+    normalChr = charExcept [marker]
+    quotedChr = fmap special $ char escape *> anyChar
+    special 'n' = '\n'
+    special 't' = '\t'
+    special c = c
 
 sexp :: P S
 sexp = whitespace *> sexp1
     where
-    sexp1 = atom <|> list
-    atom = fmap (S . Right) $ do
-        whitespace
-        num <|> str
-    num = fmap Right $ number <|> paren '<' '>' expr
-    str = fmap Left $ paren '"' '"' $ many $ do
-        char '\\' *> anyChar <|> do
-          c <- anyChar
-          guard (c /= '"')
-          return c
-    list = fmap (S . Left) $ paren '(' ')' $ do
+    sexp1 = atom <|> quote <|> list
+    
+    quote = fmap Q $ do
+        char '\''
+        sexp
+    
+    atom = fmap A $ num <|> str <|> sym
+        where
+        num = fmap Num $ number <|> paren '<' '>' expr
+        str = fmap Str $ stringEscapable '"' '\\'
+        sym = fmap Sym $ name <|> stringEscapable '|' '\\'
+        
+        name = liftA2 (:) first (some rest)
+        first = charExcept $ prohibited ++ "0123456789."
+        rest = charExcept prohibited
+        prohibited = " \t\n\\,'\"()<>[]|"
+    
+    list = fmap L $ paren '(' ')' $ do
         whitespace
         list1 <* whitespace <|> pure []
-    list1 = do
-        liftA2 (:) sexp1 $ many $ whitespace1 *> sexp1
+        where
+        list1 = do
+            liftA2 (:) sexp1 $ many $ whitespace1 *> sexp1
 
+instance Read S where
+    readsPrec _ str = case runP sexp str of
+        Right v -> [v]
+        Left e -> error e
+
+--
+
+type Frame = [(String, V)]
+addFrame name value frame = (name, value) : frame
+findFrame name frame = lookup name frame
+
+type Env = [Frame]
+addEnv frame env = frame : env
+findEnv name env = msum $ map (findFrame name) env
+
+newtype I a = I (ErrorT String (StateT Env IO) a) deriving (Monad, Functor)
+
+withFrame :: Frame -> I a -> I a
+withFrame frame (I m) = I $ ErrorT $ withStateT (frame:) (runErrorT m)
+
+lookupName :: String -> I V
+lookupName name = I $ do
+    r <- gets $ findEnv name
+    maybe (fail $ "variable |" ++ name ++ "| not found") return r
+
+runI :: Env -> I a -> IO (Either String a, Env)
+runI env (I i) = runStateT (runErrorT i) env
+
+evalI :: Env -> I a -> IO (Either String a)
+evalI env (I i) = evalStateT (runErrorT i) env
+
+ii :: I a -> IO (Either String a)
+ii = evalI []
+
+data V
+    = S S
+    | U String
+    | F Env [String] [S]
+    deriving (Show, Eq)
+
+eval :: S -> I V
+eval (Q s) = return $ S s
+eval (L xs) = do
+    r:rs <- mapM eval xs
+    apply r rs
+eval (A (Sym a)) = do
+    lookupName a
+eval s = return $ S s
+
+apply :: V -> [V] -> I V
+apply (U m) _ = fail $ ($ "") $ showString "can't apply undefined: "  . shows m
+apply (F env argnames f) args = do
+    return $ S $ A $ Sym "eval'd"
+apply (S s) _ = fail $ ($ "") $ showString "can't apply "  . shows s
